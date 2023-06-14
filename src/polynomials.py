be16c92cfc7313cb4fa0c0bb654bf63f7aeb2bdd
bches <bpchesney@gmail.com>
2023-06-13 22:21:50 -0500

    tested scale and shifts andbegan mul

--- src/polynomials.py
+++ src/polynomials.py
@@ -1,14 +1,10 @@
-from groups import exponential_group, multiplicative_group, additive_group, incremental_set
+from groups import multiplicative_group, additive_group, incremental_set
 
 
 class polynomial:
     def __init__(self, coef_grp, coefs):
         if type(coef_grp) is dict:
             self.coef_grp = coef_grp
-        elif type(coef_grp) is exponential_group:
-            self.coef_grp = {"exp":coef_grp,
-                             "mul":coef_grp.subop,
-                             "add":coef_grp.subop.subop}
         elif type(coef_grp) is multiplicative_group:
             self.coef_grp = {"mul":coef_grp,
                              "add":cpef_grp.subop}
@@ -21,8 +17,7 @@ class polynomial:
     def __repr__(self):
         s = "<Instance of %s at addr %s:\n" % (self.__class__.__name__, id(self))
         s += "\tc0*x^0 + c1*x^1 + ... + c{N-1}*x^{N-1},\n"
-        s += "\tcoefs = %s,\n" % self.coefs
-        s += "\tgroups: %s>" % self.coef_grp
+        s += "\tcoefs = %s>" % self.coefs
         return s
 
     def __len__(self):
@@ -40,11 +35,14 @@ class polynomial:
     
     def __lshift__(self, amount):
         '''Shift down (left) by amount'''
-        return polynomial(self.coef_grp, self.coefs[1:] + [0])
+        assert amount <= len(self), "Shift amount must be <= length of polynomial"
+        return polynomial(self.coef_grp, self.coefs[amount:])
 
     def __rshift__(self, amount):
         '''Shift up (right) by amount'''
-        return polynomial(self.coef_grp, [0] + self.coefs[:-1])
+        assert amount <= len(self), "Shift amount must be <= length of polynomial"
+        coefs = array('I', [0] * amount + self.coefs.tolist())
+        return polynomial(self.coef_grp, coefs)
 
     def __eq__(self, other):
         if len(self) != len(other):
@@ -72,7 +70,14 @@ class polynomial:
             result = [self.coef_grp["add"].synthesize(a,b) for a, b in zip(self,other)]
         return polynomial(self.coef_grp, result)
 
-    
+    def __mul__(self, other):
+        '''Multiply two polynomials whose coefs are of the same multiplicative group'''
+        assert self.coef_grp["mul"] == other["mul"], "polynomials must have the same multiplicative group to multiply"
+        M,N = len(self), len(other)
+        A = [(self >> n).scale(other[n]) for n in range(N)]
+        print("A=",A)
+
+        
 if __name__ == '__main__':
     from array import array as array
     from groups import binary_group
@@ -86,4 +91,9 @@ if __name__ == '__main__':
     print()
     print('p1 + p2 =', p1 + p2)
     print()
+
+    print('p2 >> 3 =', p2 >> 3)
+    print('p1 << 2 =', p1 << 2)
+    print()
+    
     print('p1 * p2 =', p1*p2)
